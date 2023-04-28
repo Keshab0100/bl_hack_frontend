1a142de96075af25fd22db240ddec2503cff5b31
Keshab <www.keshab.kk@gmail.com>
2023-04-29 02:55:39 +0530

    second commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TimelineUI from '../components/TimelineUI'
+import MyForm from '../components/Form'
+
+const Home = () => {
+  return (
+    <div>
+        <MyForm/>
+        <TimelineUI/>
+    </div>
+  )
+}
+
+export default Home
